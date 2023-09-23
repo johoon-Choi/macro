86bee4ea779109810719a5720ef1abd7a4d98fec
johon-Choi <johon4687@gmail.com>
2023-09-23 15:03:27 +0900

    문자열 입력 부분 전까지 완료

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ``` bash
 pip install selenium
 ```
+
+### popup창 제거 링크
+`https://yeko90.tistory.com/entry/%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%85%80%EB%A0%88%EB%8B%88%EC%9B%80-%EA%B8%B0%EC%B4%88-%ED%8C%9D%EC%97%85%EC%B0%BD-%EB%8B%AB%EB%8A%94-%EB%B0%A9%EB%B2%95`
--- test.py
+++ test.py
@@ -1,42 +1,70 @@
 from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 class interpark:
+    id = 'johoon4687'
+    pw = 'Hhtad9134@'
+    goodsCode = '23006740'
+
     def __init__(self):
         self.driver = webdriver.Chrome()
         # 사이즈조절
         self.driver.set_window_size(1400, 1000)  # (가로, 세로)
         self.driver.get('https://ticket.interpark.com/Gate/TPLogin.asp') # 페이지 이동
+        # 로그인 후에 페이지가 로딩될 때까지 대기
     def login(self):
         self.driver.switch_to.frame(self.driver.find_element(By.XPATH, "//div[@class='leftLoginBox']/iframe[@title='login']"))
         userId = self.driver.find_element(By.ID, 'userId')
-        userId.send_keys('johoon4687') # 로그인 할 계정 id
+        userId.send_keys(self.id) # 로그인 할 계정 id
         userPwd = self.driver.find_element(By.ID, 'userPwd')
-        userPwd.send_keys('Hhtad9134@') # 로그인 할 계정의 패스워드
+        userPwd.send_keys(self.pw) # 로그인 할 계정의 패스워드
         userPwd.send_keys(Keys.ENTER)
-        self.driver.get('http://ticket.interpark.com/Ticket/Goods/GoodsInfo.asp?GoodsCode=' + '23006740')
+        # 로그인 후에 페이지가 로딩될 때까지 대기
+        time.sleep(1)
+        self.driver.get('http://ticket.interpark.com/Ticket/Goods/GoodsInfo.asp?GoodsCode=' + self.goodsCode)
 
-    def check_exists_by_element(self, by, value):
+    # 태그가 없으면 에러발생
+    def check_exists_by_element(self, by, name):
         try:
-            self.driver.find_element(by, value)
-            return True
+            self.driver.find_element(by, name)
         except NoSuchElementException:
             return False
+        return True
     
     def close_ticketing_info(self):
-        ticketingInfo_check = self.check_exists_by_element(By.XPATH, "//*[@id="popup-prdGuide"]/div/div[3]/button")
+        # 예매안내가 팝업이 뜨면 닫기. ( ticketingInfo_check : True, False )
+        time.sleep(1)
+        ticketingInfo_check = self.check_exists_by_element(By.XPATH, "//*[@id='popup-prdGuide']/div/div[3]/button")
         if ticketingInfo_check:
-            self.driver.find_element(By.XPATH, "//*[@id="popup-prdGuide"]/div/div[3]/button").click()
+            self.driver.find_element(By.XPATH, "//*[@id='popup-prdGuide']/div/div[3]/button").click()
 
+    def click_reservation_button(self):
+        # 예매하기 버튼 클릭
+        self.driver.find_element(By.XPATH, "//*[@id='productSide']/div/div[2]/a[1]").click()
+
+        # 예매하기 눌러서 새창이 뜨면 포커스를 새창으로 변경
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        self.driver.switch_to.frame(self.driver.find_element(By.XPATH, "//*[@id='ifrmSeat']"))
+        chapchaText = self.driver.find_element(By.ID, 'txtCaptcha')
+        chapchaText.send_keys(input())
+
+    def lets_reserve(self):
+        self.driver.get_window_position(self.driver.window_handles[1])
 
 stuff = interpark()
 stuff.login()
-stuff.close_ticketing_info()
+stuff.close_ticketing_info()    
+stuff.click_reservation_button()
+stuff.lets_reserve()
 
-# 예매하기 버튼 클릭
-driver.find_element(By.XPATH, "//div[@class='tk_dt_btn_TArea']/a").click()
+# # 예매하기 버튼 클릭
+# driver.find_element(By.XPATH, "//div[@class='tk_dt_btn_TArea']/a").click()
 
-# 예매하기 눌러서 새창이 뜨면 포커스를 새창으로 변경
-driver.switch_to.window(driver.window_handles[1])
-driver.get_window_position(driver.window_handles[1])
+# # 예매하기 눌러서 새창이 뜨면 포커스를 새창으로 변경
+# driver.switch_to.window(driver.window_handles[1])
+# driver.get_window_position(driver.window_handles[1])
